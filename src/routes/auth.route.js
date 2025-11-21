419aaa1e7c4ef3d66830f3db22d118d05ed35dc1
gaurav060404 <gs762282@gmail.com>
2025-11-21 21:20:55 +0530

    add other auth controllers like refreshAccessToken,resendVerificcationEmail, verifyEmail & getCurrentUser

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,5 +1,13 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/auth.controller.js";
+import {
+  getCurrentUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+  resendEmailVerification,
+  verifyEmail,
+} from "../controllers/auth.controller.js";
 import { validate } from "../middlewares/vaildator.middleware.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import {
@@ -12,5 +20,9 @@ const router = Router();
 router.route("/register").post(registerUserValidator(), validate, registerUser);
 router.route("/login").post(loginUserValidator(), validate, loginUser);
 router.route("/logout").post(authMiddleware, logoutUser);
+router.route("/current-user").get(authMiddleware, getCurrentUser);
+router.route("/verify-email/:verificationToken").get(verifyEmail);
+router.route("/resend-email-verification").get(authMiddleware, resendEmailVerification);
+router.route("/refresh-token").get(refreshAccessToken);
 
 export default router;
