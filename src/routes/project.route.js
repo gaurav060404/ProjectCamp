2d69cfad6a47183e8fe6e56930eb300a8316faac
gaurav060404 <gs762282@gmail.com>
2025-11-24 20:05:44 +0530

    add project routes for the corresponding project controllers

--- src/routes/project.route.js
+++ src/routes/project.route.js
@@ -5,12 +5,18 @@ import {
 } from "../middlewares/auth.middleware.js";
 import { validate } from "../middlewares/vaildator.middleware.js";
 import {
+  addProjectMember,
   createProject,
+  deleteProject,
   getMembers,
   getProjectDetails,
   getProjects,
+  removeMember,
 } from "../controllers/project.controller.js";
-import { createProjectValidator } from "../validators/index.js";
+import {
+  addMemberValidator,
+  createProjectValidator,
+} from "../validators/index.js";
 
 const router = Router();
 
@@ -20,5 +26,20 @@ router
 router.route("/").get(authMiddleware, getProjects);
 router.route("/:projectId/members").get(authMiddleware, getMembers);
 router.route("/:projectId").get(authMiddleware, getProjectDetails);
+router
+  .route("/:projectId")
+  .delete(authMiddleware, adminMiddleware, deleteProject);
+router
+  .route("/:projectId/members/:userId")
+  .delete(authMiddleware, adminMiddleware, removeMember);
+router
+  .route("/:projectId/members")
+  .post(
+    authMiddleware,
+    adminMiddleware,
+    addMemberValidator(),
+    validate,
+    addProjectMember,
+  );
 
 export default router;
