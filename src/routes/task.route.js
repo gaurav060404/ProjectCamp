a5d37e19241c1fc739a734d15ec19d33d1eb0283
gaurav060404 <gs762282@gmail.com>
2025-11-25 20:30:52 +0530

    add project routes for tasks

--- src/routes/task.route.js
+++ src/routes/task.route.js
@@ -1,5 +1,23 @@
 import { Router } from "express";
+import {
+  adminOrProjectAdmin,
+  authMiddleware,
+} from "../middlewares/auth.middleware.js";
+import { createTaskValidator } from "../validators/index.js";
+import { validate } from "../middlewares/vaildator.middleware.js";
+import { createTask, getTasks } from "../controllers/task.controller.js";
 
 const router = Router();
+
+router
+  .route("/:projectId")
+  .post(
+    authMiddleware,
+    adminOrProjectAdmin,
+    createTaskValidator(),
+    validate,
+    createTask,
+  );
+router.route("/:projectId").get(authMiddleware, getTasks);
 
 export default router;
