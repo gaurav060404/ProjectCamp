a879e9bbdee705f942b18f88f08671f5e0a7140b
gaurav060404 <gs762282@gmail.com>
2025-11-25 19:47:08 +0530

    update task model to add project id

--- src/models/task.model.js
+++ src/models/task.model.js
@@ -1,27 +1,36 @@
 import mongoose, { Schema } from "mongoose";
 import { AvailableTaskStatus } from "../utils/constants";
 
-const taskSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    assignee: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: AvailableTaskStatus,
+      required: true,
+    },
+    project: {
+      type: Schema.Types.ObjectId,
+      ref: "Project",
+      required: true,
+      index: true,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  assignee: {
-    id: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: AvailableTaskStatus,
-    required: true,
-  },
-});
+  { timestamps: true },
+);
 
 export const Task = mongoose.model("Task", taskSchema);
