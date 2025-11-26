168a54dfdf0f22369fb7dcdaf0b768223fe592ec
gaurav060404 <gs762282@gmail.com>
2025-11-26 20:09:31 +0530

    add subtasks routes

--- src/routes/task.route.js
+++ src/routes/task.route.js
@@ -5,7 +5,16 @@ import {
 } from "../middlewares/auth.middleware.js";
 import { createTaskValidator } from "../validators/index.js";
 import { validate } from "../middlewares/vaildator.middleware.js";
-import { createTask, getTasks } from "../controllers/task.controller.js";
+import {
+  createSubTask,
+  createTask,
+  deleteTask,
+  getTaskDetails,
+  getTasks,
+  updateSubTask,
+  updateTask,
+} from "../controllers/task.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
@@ -14,10 +23,48 @@ router
   .post(
     authMiddleware,
     adminOrProjectAdmin,
+    upload.any(),
     createTaskValidator(),
     validate,
     createTask,
   );
 router.route("/:projectId").get(authMiddleware, getTasks);
+router.route("/:projectId/t/:taskId").get(authMiddleware, getTaskDetails);
+router
+  .route("/:projectId/t/:taskId")
+  .delete(authMiddleware, adminOrProjectAdmin, deleteTask);
+router
+  .route("/:projectId/t/:taskId")
+  .put(
+    authMiddleware,
+    adminOrProjectAdmin,
+    upload.any(),
+    createTaskValidator(),
+    validate,
+    updateTask,
+  );
+router
+  .route("/:projectId/t/:taskId/subtask")
+  .post(
+    authMiddleware,
+    adminOrProjectAdmin,
+    upload.any(),
+    createTaskValidator(),
+    validate,
+    createSubTask,
+  );
+router
+  .route("/:projectId/st/:subTaskId")
+  .delete(authMiddleware, adminOrProjectAdmin, deleteTask);
+
+router
+  .route("/:projectId/st/:subTaskId")
+  .put(
+    authMiddleware,
+    upload.any(),
+    createTaskValidator(),
+    validate,
+    updateSubTask,
+  );
 
 export default router;
