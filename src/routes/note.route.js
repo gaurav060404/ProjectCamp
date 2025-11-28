51bc7a09bc6e4e0fb2217533f89b02dbcfefe711
gaurav060404 <gs762282@gmail.com>
2025-11-28 16:40:11 +0530

    add note routes for update,delete,get and create notes

--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -1,7 +1,42 @@
 import { Router } from "express";
+import {
+  adminMiddleware,
+  authMiddleware,
+} from "../middlewares/auth.middleware.js";
+import { validate } from "../middlewares/vaildator.middleware.js";
+import {
+  createNote,
+  deleteNote,
+  getNoteDetails,
+  getNotes,
+  updateNote,
+} from "../controllers/note.controller.js";
+import { createNoteValidator } from "../validators/index.js";
 
 const router = Router();
 
-
+router
+  .route("/:projectId")
+  .post(
+    authMiddleware,
+    adminMiddleware,
+    createNoteValidator(),
+    validate,
+    createNote,
+  );
+router.route("/:projectId").get(authMiddleware, getNotes);
+router.route("/:projectId/n/:noteId").get(authMiddleware, getNoteDetails);
+router
+  .route("/:projectId/n/:noteId")
+  .put(
+    authMiddleware,
+    adminMiddleware,
+    createNoteValidator(),
+    validate,
+    updateNote,
+  );
+router
+  .route("/:projectId/n/:noteId")
+  .delete(authMiddleware, adminMiddleware, deleteNote);
 
 export default router;
